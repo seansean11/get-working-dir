ef319efaa3822ebe1648bfe56ff03866b1a08cf1
Sean Michael <seankmichael@gmail.com>
2020-05-07 21:50:50 -0600

    again

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-const gitPath = await io.which("git", true);
 const args = [
   "diff-tree",
   "--no-commit-id",
@@ -10,14 +9,19 @@ const args = [
   core.getInput("commit-hash"),
 ];
 
-try {
-  const depth = core.getInput("depth");
-  core.setOutput("working-dir", "test");
-  const test = exec.exec(`"${gitPath}"`, args);
-  core.info(test);
-  core.info(process.env);
-  core.info(`Hello ${depth}!`);
-} catch (error) {
-  core.info(error);
-  core.setFailed(error.message);
-}
+const start = async () => {
+  try {
+    const gitPath = await io.which("git", true);
+    const depth = core.getInput("depth");
+    core.setOutput("working-dir", "test");
+    const test = await exec.exec(`"${gitPath}"`, args);
+    core.info(test);
+    core.info(process.env);
+    core.info(`Hello ${depth}!`);
+  } catch (error) {
+    core.info(error);
+    core.setFailed(error.message);
+  }
+};
+
+start();
